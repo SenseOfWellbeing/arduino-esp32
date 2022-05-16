1caaf0abe520e7b31c7cb39efbadfec3399cc8a2
acabStephan <101325292+acabStephan@users.noreply.github.com>
2022-05-16 14:57:47 +0200

    fucking found the fucking error
    Now just a pin problem

--- vindrik1/vindrik_1/src/Types.h
+++ vindrik1/vindrik_1/src/Types.h
@@ -1,12 +1,7 @@
-#pragma once
-
-struct particleSensorState_t {
-    // uint16_t avgPM25 = 0;
-    __uint128_t avgPM25 = 0;
-    // uint16_t measurements[5] = {0, 0, 0, 0, 0};
-    __uint128_t measurements[5] = {0, 0, 0, 0, 0};
-    //uint8_t measurementIdx = 0;
-    __uint128_t measurementIdx = 0;
-    //boolean valid = false;
-    bool valid = false;
+struct SensorState
+{
+    uint16_t avgPM25 = 0;
+    uint16_t measurements[5] = {0, 0, 0, 0, 0};
+    uint16_t measurementIdx = 0;
+    boolean valid = false;
 };
--- vindrik1/vindrik_1/src/main.cpp
+++ vindrik1/vindrik_1/src/main.cpp
@@ -1,66 +1,44 @@
+using namespace std;
 #include <Arduino.h>
 
-#include "ssd1306.h"
+#include "SSD1306.h"
 #include "SerialCom.h"
+#include <Wire.h>
 #include "Config.h"
-#include "Types.h"
 
-// Is this necerssary
+// Is this necerssary?
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 32
 #define OLED_PIN_RESET 4
 #define OLED_ADRESS 0x78
 
+#define REPORT_DELAY 30 //seconds
+#define lmillis() ((long)millis())
+
 // WIFI
 const char* WIFI_SSID = "htl-schueler";
 const char* WIFI_PASSWORD = "20neuesWLAN17";
 
-//SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_PIN_RESET);
-
-
-particleSensorState_t state;
-uint32_t statusPublishPreviousMillis = 0;
-const uint16_t statusPublishInterval = 30000; // 30 seconds = 30000 milliseconds
+SensorState state;
+long nextReport = 0;
 
 void setup() {
-    //ssd1306_setFixedFont(ssd1306xled_font6x8);
-    //sh1106_128x64_i2c_init();
-    
-    //SSD1306::init();
-    //ssd1306_clearscreen();
-
-     Serial.begin(115200);
-    SerialCom::setup();
-
-    Config::load();
-
-    Serial.println("\n");
-    Serial.println("Hello from esp32-vindriktning-particle-sensor");
-    /*Serial.print("Core Version: %s\n", ESP.getCoreVersion().c_str());
-    Serial.print("Cores: %s\n", ESP.getChipCores().c_str());
-    Serial.print("Boot Version: %u\n", ESP.getBootVersion());
-    Serial.print("Boot Mode: %u\n", ESP.getBootMode());
-    Serial.print("CPU Frequency: %u MHz\n", ESP.getCpuFreqMHz());
-    Serial.print("Reset reason: %s\n", ESP.getResetReason().c_str());*/
-
-    Serial.println("-- Current GPIO Configuration --");
-    Serial.println("PIN_UART_RX: %d\n");
-    Serial.println(SerialCom::PIN_UART_RX);
+  Serial.begin(115200);
+  SerialCom::setup();
+  nextReport = lmillis() + REPORT_DELAY * 1000;
 }
 
 void loop() {
-  //delay(1000);
-    //ssd1306_clearScreen();
-    SerialCom::handleUart(state);
-    const uint32_t currentMillis = millis();
-    if (currentMillis - statusPublishPreviousMillis >= statusPublishInterval) {
-        statusPublishPreviousMillis = currentMillis;
-
-        if (state.valid) {
-            Serial.print("Publish state\n");
-            //publishState(); -> Print to MQTT
-        }
-    }
-
-
+  SerialCom::handleUart(state);
+  // if(lmillis() - nextReport >= 0){
+      // nextReport = lmillis() + REPORT_DELAY; //* 1000;
+        delay(100);
+      // if(state.valid){
+        Serial.println("Wert: " + state.avgPM25);
+      // }
+      // else{
+        // Serial.println("---");
+        // Serial.println(nextReport + " " + lmillis());
+      // }
+  // }
 }
