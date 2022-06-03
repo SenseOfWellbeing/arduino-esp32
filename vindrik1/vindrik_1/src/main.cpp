a3e26835ac17e4f933143f96ef5060449e05f60b
acabStephan <101325292+acabStephan@users.noreply.github.com>
2022-06-03 11:47:50 +0200

    Tried to send sensor values over mqtt

--- vindrik1/vindrik_1/src/main.cpp
+++ vindrik1/vindrik_1/src/main.cpp
@@ -1,32 +1,62 @@
 using namespace std;
 #include <Arduino.h>
-
-#include "SSD1306.h"
+#include <Wifi.h>
+#include <PubSubClient.h>
 #include "SerialCom.h"
-#include <Wire.h>
 #include "Config.h"
-
-// Is this necerssary?
-#define OLED_WIDTH 128
-#define OLED_HEIGHT 32
-#define OLED_PIN_RESET 4
-#define OLED_ADRESS 0x78
 
 #define REPORT_DELAY 30 //seconds
 #define lmillis() ((long)millis())
 
 // WIFI
-const char* WIFI_SSID = "htl-schueler";
-const char* WIFI_PASSWORD = "20neuesWLAN17";
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const char* client_id = "fe514e7d-a92d-4661-9723-a7a5d99ebb52";
+const char* ssid = "htl-schueler";
+const char ssid_arr[] = "htl-schueler";
+
+const char* wifikey = "20neuesWLAN17";
+const char wifikey_arr[] = "20neuesWLAN17";
+const char* topic = "temp";
+const char* srvIp = "172.17.243.87";
+
+const char* username = "SenseOfWellbeing";
+const char* password = "admin01";
+
+//char airQual[50];
+//string airQual_str ="";
 
 SensorState state;
 long nextReport = 0;
+
+void setupWifi(){
+  delay(10);
+  Serial.println("Connecting to:");
+  Serial.print(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.println(".");
+  }
+
+  Serial.println("WiFi connected");
+  Serial.println("IP Adress: ");
+  Serial.print(WiFi.localIP());
+}
 
 void setup() {
   pinMode(13,INPUT);
   Serial.begin(115200);
   
   SerialCom::setup();
+
+  setupWifi();
+  client.setServer(srvIp, 1883);
+  client.connect(client_id, ssid_arr, wifikey_arr);
+
   nextReport = lmillis() + REPORT_DELAY * 1000;
 }
 
@@ -37,6 +67,11 @@ void loop() {
         delay(1000);
       if(state.valid){
         Serial.println(state.avgPM25);
+        //String(state.avgPM25).toCharArray(airQual, String(state.avgPM25).length()+1);
+        //client.publish(topic, airQual);
+        client.publish(topic, state.avgPM25, String(state.avgPM25).length()); 
+        // client.publish(topic, state.avgPM25);
+        // client.publish()
       }
       else{
         Serial.println("---");
